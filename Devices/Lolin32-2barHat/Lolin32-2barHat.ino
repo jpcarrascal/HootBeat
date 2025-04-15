3f999544c99cdc0f5d60583acfc0b487beed6691
JP <jp@JP-M1-2439.local>
2025-04-15 14:34:02 +0200

    Fixed an error in Lolin32-2barHat

--- Devices/Lolin32-2barHat/Lolin32-2barHat.ino
+++ Devices/Lolin32-2barHat/Lolin32-2barHat.ino
@@ -13,30 +13,34 @@
  #define NUMLEDS 5
  
  
- uint8_t anim = 10;
+ uint8_t disconnAnim = 10;
+ uint8_t connAnim = 0;
+ uint8_t anim = disconnAnim;
  uint8_t r1=0, g1=0, b1=0;
  uint8_t r2=0, g2=0, b2=0;
  uint32_t connColor      = 0x080808,
-          disconnColor   = 0x080000,
+          disconnColor   = 0xFF0000,
           bdColor        = 0x0044FF,
           sdColor        = 0xFF0000;
  
- HootBeat hb = HootBeat(NUMLEDS, PINL, PINR);
+ HootBeat hbL = HootBeat(NUMLEDS, PINL);
+ HootBeat hbR = HootBeat(NUMLEDS, PINR);
  
  void setup() {
    Serial.begin(115200);
-   
+   hbL.setDelay(0);
+   hbR.setDelay(0);
    BLEMidiServer.begin("2barHat");
    BLEMidiServer.setOnConnectCallback([](){
      Serial.println("Connected");
-     hb.setColor(connColor);
-     anim = 10;
+     hbL.setColor(connColor); hbR.setColor(connColor);
+     anim = connAnim;
    });
    BLEMidiServer.setOnDisconnectCallback([](){ // callback with a lambda function
      Serial.println("Disconnected");
-     hb.setColor(disconnColor);
-     hb.isRunning = true;
-     anim = 10;
+     hbL.setColor(disconnColor); hbR.setColor(disconnColor);
+     hbL.isRunning = true;       hbR.isRunning = true;
+     anim = disconnAnim;
    });
    BLEMidiServer.setNoteOnCallback(onNoteOn);
    BLEMidiServer.setNoteOffCallback(onNoteOff);
@@ -49,25 +53,26 @@
    if (BLEMidiServer.isConnected()) {
  
    }
-   hb.step(anim);
+   hbL.step(anim); hbR.step(anim);
+   delay(40);
  }
  
  // MIDI Callbacks:
  
  void onNoteOn(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timestamp)
  {
-   hb.isRunning = true;
+   hbL.isRunning = true; hbR.isRunning = true;
    if(velocity > 0) {// && hb.drums) {
      if(note == 36 && bdColor > 0) {
-       hb.setColor(bdColor);
-       hb.triggerFlash();  
+       hbL.setColor(bdColor); hbR.setColor(bdColor);
+       hbL.triggerFlash();    hbR.triggerFlash();  
      }
      else if(note == 38 && sdColor > 0) {
-       hb.setColor(sdColor);
-       hb.triggerFlash();
+       hbL.setColor(sdColor); hbR.setColor(sdColor);
+       hbL.triggerFlash();    hbR.triggerFlash();
      } else if(note == 49) {
-       hb.setColor(bdColor);
-       hb.triggerFlash(24);
+       hbL.setColor(bdColor); hbR.setColor(bdColor);
+       hbL.triggerFlash(24);  hbR.triggerFlash(24);
      }
    }
    Serial.printf("Received note on : channel %d, note %d, velocity %d (timestamp %dms)\n", channel, note, velocity, timestamp);
@@ -86,15 +91,15 @@
      g1 = value*2;
    if(controller == 104)
      b1 = value*2;
-   bdColor = hb.rgb2color(r1, g1, b1);
-   hb.setColor(r1, g1, b1);
+   bdColor = hbL.rgb2color(r1, g1, b1);
+   hbL.setColor(r1, g1, b1); hbR.setColor(r1, g1, b1);
    if(controller == 105)
      r2 = value*2;
    if(controller == 106)
      g2 = value*2;
    if(controller == 107)
      b2 = value*2;
-   sdColor = hb.rgb2color(r2, g2, b2);
+   sdColor = hbL.rgb2color(r2, g2, b2);
    //Serial.printf("Received control change : channel %d, controller %d, value %d (timestamp %dms)\n", channel, controller, value, timestamp);
    if(controller == 102) {
      Serial.print("BD color: #");
@@ -105,17 +110,32 @@
      Serial.println(sdColor, HEX);
    }
    if(controller == 111) {
-     hb.isRunning = true;
+     hbL.isRunning = true; hbR.isRunning = true;
      float fade = (float) value / 127;
-     hb.dim(fade);
+     hbL.dim(fade); hbR.dim(fade);
    }
  }
  
  void onProgramChange(uint8_t channel, uint8_t value, uint16_t timestamp)
  {
-     hb.isRunning = false;
+     hbL.isRunning = false; hbR.isRunning = false;
      anim = value;
      Serial.print("Anim: ");
      Serial.println(anim);
  }
+ 
+ /*
+ Index	Animation Name
+0	All LEDs Off
+1	All LEDs On
+2	Pulsating
+3	Pulsating + Rotating
+4	Rotating (no drums)
+5	Drums Only
+6	Alternating Colors
+7	Strobe
+8	Rotating And Drums
+9	Some On
+10	Bounce
+ */
  
